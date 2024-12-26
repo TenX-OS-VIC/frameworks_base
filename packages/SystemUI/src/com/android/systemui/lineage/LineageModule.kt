e863ee17613928ebcb0ef2ed9f3890eea71e54a6
kshitij-vk <koyande72@gmail.com>
2024-12-26 16:37:03 +0530

    Revert "SystemUI: Introduce preferred network tile"
    This reverts commit c5f283ea8fef0957c965cea4084d4f9409f3727a.
    
    Change-Id: Ie3852e14c54e09aa08d0142d73d7c6917a0fe418

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -30,7 +30,6 @@ import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
-import com.android.systemui.qs.tiles.PreferredNetworkTile
 
 import dagger.Binds
 import dagger.Module
@@ -116,10 +115,4 @@ interface LineageModule {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
-
-    /** Inject PreferredNetworkTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(PreferredNetworkTile.TILE_SPEC)
-    fun bindPreferredNetworkTile(preferredNetworkTile: PreferredNetworkTile): QSTileImpl<*>
 }
